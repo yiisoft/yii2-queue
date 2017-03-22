630f7ce3a4bf1baec1f9d1d6eff112f8f82a4ce0
Roman Zhuravlev <zhuravljov@gmail.com>
2017-03-22 12:09:40 +0200

    License BSD 3-clause

--- src/debug/Panel.php
+++ src/debug/Panel.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://github.com/zhuravljov/yii2-queue
+ * @copyright Copyright (c) 2017 Roman Zhuravlev
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
 
 namespace zhuravljov\yii\queue\debug;
 
--- tests/beanstalk/DriverTest.php
+++ tests/beanstalk/DriverTest.php
@@ -1,9 +1,13 @@
 <?php
+/**
+ * @link https://github.com/zhuravljov/yii2-queue
+ * @copyright Copyright (c) 2017 Roman Zhuravlev
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
 
 namespace tests\beanstalk;
 
 use Yii;
-use tests\Process;
 use tests\DriverTestCase;
 
 /**
