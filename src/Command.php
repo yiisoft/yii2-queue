9e4831e9571141e3e84a07952f1b00fc496eed99
Roman Zhuravlev <zhuravljov@gmail.com>
2017-04-20 13:04:28 +0300

    #37: Fixed opening of a child process

--- src/Command.php
+++ src/Command.php
@@ -93,8 +93,7 @@ abstract class Command extends Controller
             '{verbose}' => (int) $this->verbose,
         ]);
         $descriptors = [['pipe', 'r'], ['pipe', 'w'], ['pipe', 'w']];
-        $cwd = $_SERVER['PWD'];
-        $process = proc_open($cmd, $descriptors, $pipes, $cwd);
+        $process = proc_open($cmd, $descriptors, $pipes);
         if (is_resource($process)) {
             // Writes message to stdIn of process
             fwrite($pipes[0], $message);
