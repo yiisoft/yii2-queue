405d1d87a6eb613c30b65aa48ca3e2f13c81fb23
Roman Zhuravlev <zhuravljov@gmail.com>
2017-05-08 19:32:52 +0300

    #23: Retryable interface

---
+++ src/RetryableJob.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link https://github.com/zhuravljov/yii2-queue
+ * @copyright Copyright (c) 2017 Roman Zhuravlev
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace zhuravljov\yii\queue;
+
+/**
+ * Interface RetryableJob
+ *
+ * @author Roman Zhuravlev <zhuravljov@gmail.com>
+ */
+interface RetryableJob extends Job
+{
+    /**
+     * @return int time to run in seconds
+     */
+    public function getTtr();
+
+    /**
+     * @param int $attempt number
+     * @param \Exception $error from last execute of the job
+     * @return bool
+     */
+    public function canRetry($attempt, $error);
+}
