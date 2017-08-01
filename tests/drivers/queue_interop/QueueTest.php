6b89628b47f7d1a9f20f8197dd55a2bbd5a44af1
Alexander Kozienko <pascalamsg@gmail.com>
2017-08-01 13:49:29 +0300

    queue interop

---
+++ tests/drivers/queue_interop/QueueTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tests\drivers\queue_interop;
+
+use tests\drivers\CliTestCase;
+use yii\queue\queue_interop\Queue;
+
+class QueueTest extends CliTestCase
+{
+    /**
+     * @return Queue
+     */
+    protected function getQueue()
+    {
+        return \Yii::$app->interopQueue;
+    }
+
+    public function testRun()
+    {
+        // Not supported
+    }
+
+    public function testStatus()
+    {
+        // Not supported
+    }
+
+    public function testLater()
+    {
+        // Not supported
+    }
+
+    public function testRetry()
+    {
+        // Limited support
+    }
+}
