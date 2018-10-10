614a886e6b53ebe6aba8662440ef3f9a0c1836d2
Carsten Brandt <mail@cebe.cc>
2018-10-10 21:02:13 +0200

    Update Command.php

--- src/drivers/stomp/Command.php
+++ src/drivers/stomp/Command.php
@@ -14,6 +14,7 @@ use yii\queue\cli\Command as CliCommand;
  * Manages application stomp-queue.
  *
  * @author Sergey Vershinin <versh23@gmail.com>
+ * @since 2.1.1
  */
 class Command extends CliCommand
 {
