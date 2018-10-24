79175dd3f2eb9c0f34b21e3ac2239a25b80dfded
Mikk Tendermann <mikk150@gmail.com>
2018-10-24 15:58:46 +0300

    cannot have null values

--- src/drivers/db/migrations/M181024151600DelayToExecuteAt.php
+++ src/drivers/db/migrations/M181024151600DelayToExecuteAt.php
@@ -22,10 +22,11 @@ class M181024151600DelayToExecuteAt extends Migration
 
     public function up()
     {
-        $this->addColumn($this->tableName, 'execute_at', $this->integer()->notNull()->after('ttr'));
+        $this->addColumn($this->tableName, 'execute_at', $this->integer()->after('ttr'));
         $this->update($this->tableName, [
             'execute_at' => new Expression('[[pushed_at]] + [[delay]]')
         ]);
+        $this->alterColumn($this->tableName, 'execute_at', $this->integer()->notNull()->after('ttr'));
         $this->dropColumn($this->tableName, 'delay');
         $this->createIndex('idx_queue__channel__execute_at', $this->tableName, ['channel', 'execute_at']);
     }
@@ -33,10 +34,11 @@ class M181024151600DelayToExecuteAt extends Migration
     public function down()
     {
         $this->dropIndex('idx_queue__channel__execute_at', $this->tableName);
-        $this->addColumn($this->tableName, 'delay', $this->integer()->notNull()->after('ttr'));
+        $this->addColumn($this->tableName, 'delay', $this->integer()->after('ttr'));
         $this->update($this->tableName, [
             'delay' => new Expression('[[execute_at]] - [[pushed_at]]')
         ]);
+        $this->alterColumn($this->tableName, 'delay', $this->integer()->notNull()->after('ttr'));
         $this->dropColumn($this->tableName, 'execute_at');
     }
 }
