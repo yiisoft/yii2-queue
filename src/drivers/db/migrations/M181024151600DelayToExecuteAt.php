2b2cbda22874e2a3cb5959c08a9ebd3e11b6c601
Mikk Tendermann <mikk150@gmail.com>
2018-10-24 16:08:47 +0300

    this should not matter

--- src/drivers/db/migrations/M181024151600DelayToExecuteAt.php
+++ src/drivers/db/migrations/M181024151600DelayToExecuteAt.php
@@ -22,23 +22,59 @@ class M181024151600DelayToExecuteAt extends Migration
 
     public function up()
     {
-        $this->addColumn($this->tableName, 'execute_at', $this->integer()->after('ttr'));
-        $this->update($this->tableName, [
-            'execute_at' => new Expression('[[pushed_at]] + [[delay]]')
-        ]);
-        $this->alterColumn($this->tableName, 'execute_at', $this->integer()->notNull()->after('ttr'));
-        $this->dropColumn($this->tableName, 'delay');
+        $this->dropIndex('channel', $this->tableName, 'channel');
+        if ($this->db->driverName !== 'sqlite') {
+            $this->addColumn($this->tableName, 'execute_at', $this->integer()->notNull()->after('ttr'));
+            $this->update($this->tableName, [
+                'execute_at' => new Expression('[[pushed_at]] + [[delay]]')
+            ]);
+            $this->dropColumn($this->tableName, 'delay');
+        } else {
+            $this->dropTable($this->tableName);
+            $this->createTable($this->tableName, [
+                'id' => $this->primaryKey(),
+                'channel' => $this->string()->notNull(),
+                'job' => $this->binary()->notNull(),
+                'pushed_at' => $this->integer()->notNull(),
+                'ttr' => $this->integer()->notNull(),
+                'execute_at' => $this->integer()->notNull(),
+                'priority' => $this->integer()->unsigned()->notNull()->defaultValue(1024),
+                'reserved_at' => $this->integer(),
+                'attempt' => $this->integer(),
+                'done_at' => $this->integer(),
+            ]);
+            $this->createIndex('reserved_at', $this->tableName, 'reserved_at');
+            $this->createIndex('priority', $this->tableName, 'priority');
+        }
         $this->createIndex('idx_queue__channel__execute_at', $this->tableName, ['channel', 'execute_at']);
     }
 
     public function down()
     {
-        $this->dropIndex('idx_queue__channel__execute_at', $this->tableName);
-        $this->addColumn($this->tableName, 'delay', $this->integer()->after('ttr'));
-        $this->update($this->tableName, [
-            'delay' => new Expression('[[execute_at]] - [[pushed_at]]')
-        ]);
-        $this->alterColumn($this->tableName, 'delay', $this->integer()->notNull()->after('ttr'));
-        $this->dropColumn($this->tableName, 'execute_at');
+        if ($this->db->driverName !== 'sqlite') {
+            $this->dropIndex('idx_queue__channel__execute_at', $this->tableName);
+            $this->addColumn($this->tableName, 'delay', $this->integer()->notNull()->after('ttr'));
+            $this->update($this->tableName, [
+                'delay' => new Expression('[[execute_at]] - [[pushed_at]]')
+            ]);
+            $this->dropColumn($this->tableName, 'execute_at');
+        } else {
+            $this->dropTable($this->tableName);
+            $this->createTable($this->tableName, [
+                'id' => $this->primaryKey(),
+                'channel' => $this->string()->notNull(),
+                'job' => $this->binary()->notNull(),
+                'pushed_at' => $this->integer()->notNull(),
+                'ttr' => $this->integer()->notNull(),
+                'delay' => $this->integer()->notNull(),
+                'priority' => $this->integer()->unsigned()->notNull()->defaultValue(1024),
+                'reserved_at' => $this->integer(),
+                'attempt' => $this->integer(),
+                'done_at' => $this->integer(),
+            ]);
+            $this->createIndex('reserved_at', $this->tableName, 'reserved_at');
+            $this->createIndex('priority', $this->tableName, 'priority');
+        }
+        $this->createIndex('channel', $this->tableName, 'channel');
     }
 }
