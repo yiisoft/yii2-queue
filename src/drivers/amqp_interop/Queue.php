3105e04be2308eba64131e781e64f22dade48111
Kaarel <kaarel06@gmail.com>
2020-06-09 11:58:04 +0300

    Fix #380: Fixed amqp-interop queue/listen signal handling

--- src/drivers/amqp_interop/Queue.php
+++ src/drivers/amqp_interop/Queue.php
@@ -223,6 +223,28 @@ class Queue extends CliQueue
         Event::on(BaseApp::class, BaseApp::EVENT_AFTER_REQUEST, function () {
             $this->close();
         });
+
+        if (extension_loaded('pcntl') && PHP_MAJOR_VERSION >= 7) {
+            // https://github.com/php-amqplib/php-amqplib#unix-signals
+            $signals = [SIGTERM, SIGQUIT, SIGINT, SIGHUP];
+            
+            foreach ($signals as $signal) {
+                $oldHandler = null;
+                // This got added in php 7.1 and might not exist on all supported versions
+                if (function_exists('pcntl_signal_get_handler')) {
+                    $oldHandler = pcntl_signal_get_handler($signal);
+                }
+
+                pcntl_signal($signal, static function ($signal) use ($oldHandler) {
+                    if ($oldHandler && is_callable($oldHandler)) {
+                        $oldHandler($signal);
+                    }
+
+                    pcntl_signal($signal, SIG_DFL);
+                    posix_kill(posix_getpid(), $signal);
+                });
+            }
+        }
     }
 
     /**
