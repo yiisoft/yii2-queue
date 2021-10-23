88caeb611b1753832194d9215a321422f545d536
Evgeniy Moiseenko <s1lverhron@gmail.com>
2021-10-23 10:22:27 +0300

    Fix #435: Added the ability to set optional arguments for the AMQP queue

--- src/drivers/amqp_interop/Queue.php
+++ src/drivers/amqp_interop/Queue.php
@@ -169,6 +169,20 @@ class Queue extends CliQueue
      */
     public $queueName = 'interop_queue';
     /**
+     * Setting optional arguments for the queue (key-value pairs)
+     * ```php
+     * [
+     *    'x-expires' => 300000,
+     *    'x-max-priority' => 10
+     * ]
+     * ```
+     *
+     * @var array
+     * @since 2.3.3
+     * @see https://www.rabbitmq.com/queues.html#optional-arguments
+     */
+    public $queueOptionalArguments = [];
+    /**
      * The exchange used to publish messages to.
      *
      * @var string
@@ -412,7 +426,10 @@ class Queue extends CliQueue
 
         $queue = $this->context->createQueue($this->queueName);
         $queue->addFlag(AmqpQueue::FLAG_DURABLE);
-        $queue->setArguments(['x-max-priority' => $this->maxPriority]);
+        $queue->setArguments(array_merge(
+            ['x-max-priority' => $this->maxPriority],
+            $this->queueOptionalArguments
+        ));
         $this->context->declareQueue($queue);
 
         $topic = $this->context->createTopic($this->exchangeName);
