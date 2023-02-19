2c1ef0abc4880e8cb7b059fec0d46d9724797f67
s1lver <s1lverhron@gmail.com>
2023-02-19 22:42:56 +0300

    Fixed running test for php 8.0

--- tests/docker/php/entrypoint-php80.sh
+++ tests/docker/php/entrypoint-php80.sh
@@ -3,24 +3,13 @@
 set -eu
 
 flock tests/runtime/composer-install.lock composer install --prefer-dist --no-interaction --ignore-platform-req=php
+composer require phpunit/phpunit:~7.5.20 --ignore-platform-req=php --dev --with-all-dependencies --no-interaction
 
 tests/yii sqlite-migrate/up --interactive=0
 
-tests/docker/wait-for-it.sh mysql:3306 -t 180
 tests/docker/php/mysql-lock.php tests/yii mysql-migrate/up --interactive=0
 
-tests/docker/wait-for-it.sh postgres:5432 -t 180
 tests/docker/php/mysql-lock.php tests/yii pgsql-migrate/up --interactive=0
-
-tests/docker/wait-for-it.sh redis:6379 -t 180
-
-tests/docker/wait-for-it.sh rabbitmq:5672 -t 180
-
-tests/docker/wait-for-it.sh beanstalk:11300 -t 180
-
-tests/docker/wait-for-it.sh gearmand:4730 -t 180
-
-tests/docker/wait-for-it.sh activemq:61613 -t 180
 
 php --version
 set -x
