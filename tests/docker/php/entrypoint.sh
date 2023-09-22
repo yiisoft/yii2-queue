d8659d5a5aa2d9259d95b8a20c590d6a28afa0c5
Moiseenko <s1lverhron@gmail.com>
2023-09-22 20:43:57 +0400

    opis/closure replaced to laravel/serializable-closure

--- tests/docker/php/entrypoint.sh
+++ tests/docker/php/entrypoint.sh
@@ -5,9 +5,9 @@ set -eu
 flock tests/runtime/composer-install.lock composer install --prefer-dist --no-interaction
 
 tests/yii sqlite-migrate/up --interactive=0
-sleep 60
+sleep 30
 tests/docker/php/mysql-lock.php tests/yii mysql-migrate/up --interactive=0
-sleep 60
+sleep 30
 tests/docker/php/mysql-lock.php tests/yii pgsql-migrate/up --interactive=0
 
 php --version
