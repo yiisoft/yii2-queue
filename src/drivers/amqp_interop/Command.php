0668bc39da691aca2b0adbd121d13cf2be641ce9
Kirill Ka <snewer0@gmail.com>
2026-03-04 03:37:25 +0400

    #552 Move signal handling from queue component to controller

--- src/drivers/amqp_interop/Command.php
+++ src/drivers/amqp_interop/Command.php
@@ -29,6 +29,37 @@ class Command extends CliCommand
     /**
      * @inheritdoc
      */
+    public function init(): void
+    {
+        parent::init();
+
+        // https://github.com/yiisoft/yii2-queue/issues/379
+        if (PHP_MAJOR_VERSION >= 7 && extension_loaded('pcntl') && function_exists('pcntl_signal')) {
+            // https://github.com/php-amqplib/php-amqplib#unix-signals
+            $signals = [SIGTERM, SIGQUIT, SIGINT, SIGHUP];
+
+            foreach ($signals as $signal) {
+                $oldHandler = null;
+                // This got added in php 7.1 and might not exist on all supported versions
+                if (function_exists('pcntl_signal_get_handler')) {
+                    $oldHandler = pcntl_signal_get_handler($signal);
+                }
+
+                pcntl_signal($signal, static function ($signal) use ($oldHandler) {
+                    if ($oldHandler && is_callable($oldHandler)) {
+                        $oldHandler($signal);
+                    }
+
+                    pcntl_signal($signal, SIG_DFL);
+                    posix_kill(posix_getpid(), $signal);
+                });
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function isWorkerAction($actionID): bool
     {
         return $actionID === 'listen';
